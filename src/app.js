316e1eb908b6881fc6e3c1c0e2b2e995b79c3da2
PhDBui <congdoan6997@gmail.com>
2023-11-15 22:36:14 +0300

    section 2 init project and install npm important

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const compression = require('compression');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan');
+const app = express();
+
+// init middlewares
+// app.use(morgan('dev'));
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(compression());
+// init db
+
+// init routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Hello World'.repeat(100000)
+    })
+})
+
+
+// init error handler
+
+module.exports = app;
