2933bae2e667f6e71e740e0f078ca4a021543ff9
PhDBui <congdoan6997@gmail.com>
2023-11-25 23:31:49 +0300

    section 12 create model, controller, router, service for product

---
+++ src/models/product.model.js
@@ -0,0 +1,111 @@
+const { model, Schema } = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Product';
+const COLLECTION_NAME = 'Products';
+// Declare the Schema of the Mongo model
+var productSchema = new Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_thumb: {
+      type: String,
+      required: true,
+    },
+    product_description: {
+      type: String,
+    },
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    product_quantity: {
+      type: Number,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ['Electronics', 'Furniture', 'Grocery', 'Clothing', 'Others'],
+    },
+    product_user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    product_attributes: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+var electronicSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    weight: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'Electronics',
+  }
+);
+
+var furnitureSchema = new Schema(
+  {
+    color: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'Furniture',
+  }
+);
+
+var clothingSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    material: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'Clothes',
+  }
+);
+//Export the model
+module.exports = {
+  product: model(DOCUMENT_NAME, productSchema),
+  electronic: model('Electronic', electronicSchema),
+  furniture: model('Furniture', furnitureSchema),
+  clothing: model('Clothing', clothingSchema),
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,9 +8,10 @@ const { apiKey, permissionKey } = require('../auth/checkAuth');
 router.use(apiKey);
 
 // check permission
-// router.use(permissionKey('0000'));
+router.use(permissionKey('0000'));
 
 // routes
 router.use('/v1/api', require('./access'));
+router.use('/v1/api/product', require('./product'));
 
 module.exports = router;
